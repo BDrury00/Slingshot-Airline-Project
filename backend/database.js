cd0ed383eb572d998e662702ed1202d776c8440b
Brandon Drury <brandondru00@gmail.com>
2023-03-18 20:08:35 -0400

    getFlights updates

---
+++ backend/database.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require("mongodb");
+const flights = require("./data.js");
+const uri =
+  "mongodb+srv://slingair:bestairline@cluster0.50bpam6.mongodb.net/Slingair_DB?retryWrites=true&w=majority";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// connect to the database
+const dbFunction = async (dbName) => {
+  const client = new MongoClient(uri, options);
+
+  try {
+    await client.connect();
+    console.log("Connected successfully to server - database.js");
+    // connect to the database
+    const db = client.db(dbName);
+    // connect to the collection
+    const collection = db.collection("AllFlightInfo");
+    console.log("Connected successfully to collection - database.js");
+    // insert the data into the database
+    const promises = Object.entries(flights).map(async ([datatype, flight]) => {
+      // check if the datatype already exists in the database
+      const existingDoc = await collection.findOne({ datatype });
+      if (existingDoc) {
+        console.log(`Skipping ${datatype} - already exists in database`);
+        return;
+      }
+      // insert the new document into the database
+      await collection.insertOne({ datatype, flight });
+      console.log(`Inserted ${datatype} into database successfully`);
+    });
+    await Promise.all(promises);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  // close the connection
+  await client.close();
+  console.log("Disconnected from server");
+};
+
+module.exports = dbFunction;
--- backend/handlers.js
+++ backend/handlers.js
@@ -1,10 +1,45 @@
 "use strict";
+//imports
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://slingair:bestairline@cluster0.50bpam6.mongodb.net/Slingair_DB?retryWrites=true&w=majority";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const client = new MongoClient(uri, options);
+
+const dbFunction = require("./database.js");
+//________________________________________________________________
 
 // use this package to generate unique ids: https://www.npmjs.com/package/uuid
 const { v4: uuidv4 } = require("uuid");
 
 // returns an array of all flight numbers
-const getFlights = (req, res) => {};
+const getFlights = async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server - getflights handler");
+
+    const db = client.db("Slingair_DB");
+    const collection = db.collection("AllFlightInfo");
+    const documents = await collection.find({}).toArray();
+
+    const flightNumbers = documents.reduce((acc, doc) => {
+      Object.keys(doc.flight).forEach((key) => {
+        if (!isNaN(key)) return; // skip index keys
+        acc.push(key);
+      });
+      return acc;
+    }, []);
+
+    client.close();
+    res.status(200).json({ flightNumbers });
+    console.log(flightNumbers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error retrieving flight numbers" });
+  }
+};
 
 // returns all the seats on a specified flight
 const getFlight = (req, res) => {};
@@ -25,11 +60,11 @@ const updateReservation = (req, res) => {};
 const deleteReservation = (req, res) => {};
 
 module.exports = {
-    getFlights,
-    getFlight,
-    getReservations,
-    addReservation,
-    getSingleReservation,
-    deleteReservation,
-    updateReservation,
+  getFlights,
+  getFlight,
+  getReservations,
+  addReservation,
+  getSingleReservation,
+  deleteReservation,
+  updateReservation,
 };
